6d63fd44b4cafb247de8c37e4e42e71e4c29ce13
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2023-10-21 07:57:23 +0530

    font styles changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,22 @@ import React from 'react'
 const Header = () => {
   return (
     <header className='flex justify-between items-center px-4 py-2'>
-        <div className='text-[#0188A6]'>
-            <h1 className='text-3xl font-black'>Naukri<span className='text-base font-black'>hub</span></h1>
+        <div className=''>
+            <h1 className='text-3xl font-black'>Naukri<span className='text-base font-black text-[#0188A6]'>hub</span></h1>
         </div>
         <nav className='hidden md:block'>
-            <ul className='flex gap-x-4 items-center'>
-                <li><span className= "font-semibold" >Home</span></li>
-                <li><span className= "font-semibold" >Govt. Jobs</span></li>
-                <li><span className= "font-semibold" >Exam Preparation</span></li>
-                <li><span className= "font-semibold" >Admit Cards</span></li>
-                <li><span className= "font-semibold" >Results</span></li> 
-                <li><span className= "font-semibold" >About Us</span></li>
+            <ul className='flex gap-x-4 items-center font-Roboto'>
+                <li><span className= "text-sm font-bold " >Home</span></li>
+                <li><span className= "text-sm font-bold " >Govt. Jobs</span></li>
+                <li><span className= "text-sm font-bold " >Admit Cards</span></li>
+                <li><span className= "text-sm font-bold " >Results</span></li> 
+                <li><span className= "text-sm font-bold " >About Us</span></li>
             </ul>
 
         </nav>
 
         <div className=''>
-            <button className='bg-[#0188A6] text-white text-sm px-4 py-2 rounded-md'>Subscribe</button>
+            <button className='bg-[#0188A6] text-white text-sm px-4 py-2 rounded-md font-Roboto'>Subscribe</button>
         </div>
 
     </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,15 @@ const Hero = () => {
   return (
     <div className="max-w-7xl px-4 py-6 md:py-6 mx-auto">
       <section className="flex justify-between flex-wrap gap-y-6">
-        <div className="self-center space-y-3 flex-1">
-          <h1 className="text-5xl font-bold">Hundreds of jobs for you</h1>
-          <h1 className="text-[#0188A6] text-5xl font-bold">Verified by us.</h1>
+        <div className="self-center space-y-4 flex-1">
+          <div className="space-y-2">
+            <h1 className="text-5xl font-bold">Hundreds of jobs for you</h1>
+            <h1 className="text-[#0188A6] text-5xl font-bold">
+              Verified by us.
+            </h1>
+          </div>
 
-          <p className="text-lg text-gray-600 font-semibold ">
+          <p className="text-lg text-gray-600 font-semibold font-Poppins">
             Here you can find your best job. Explore hundreds of jobs with us.
             Join with us and find your best dream jobs.
           </p>
@@ -20,10 +24,12 @@ const Hero = () => {
                 <input
                   type="text"
                   placeholder="e.g. Defense, Banking"
-                  className="bg-white outline-none border-none rounded-md pl-2 py-2 placeholder:text-sm w-60"
+                  className="bg-white outline-none border-none rounded-md pl-2 py-2 placeholder:text-sm placeholder:font-Poppins w-60"
                 />
               </div>
-              <button className="text-white bg-[#0188A6] border-none text-sm px-3 rounded-r-md">Search</button>
+              <button className="font-Roboto text-white bg-[#0188A6] border-none text-sm px-3 rounded-r-md">
+                Search
+              </button>
             </div>
           </form>
         </div>
