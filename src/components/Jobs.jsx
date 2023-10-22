63a6c6156beb410bf3cdc1487199e5ef9391b20f
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2023-10-22 14:41:07 +0530

    listItems created

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { jobsapi } from '../utils/constants'
+import ListItem from './ListItem'
 
 
 const Jobs = () => {
   return (
     <section className=''>
-      <h1 className='text-2xl font-bold py-4 text-center'>Latest Jobs</h1>
+      <h1 className='text-3xl font-bold py-4 text-center'>Latest <span className='text-secondary'>Jobs</span></h1>
 
       <div className=''>
+        <ul className='space-y-2'>
+          {
+            jobsapi.latestJobs.jobsList.map( (item, index) => <ListItem key={index} data={item} />)
+          }
+        </ul>
         
       </div>
 
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ListItem = ({ data }) => {
+  const { postName, lastDateToApply, numberOfPosts } = data;
+  return (
+    <li className="p-3 sm:p-4 border border-secondary rounded-lg">
+      <div className="flex justify-between items-center">
+        <div className="inline-flex flex-col">
+          <h2 className="font-semibold">{postName}</h2>
+          <span className="font-light italic text-primary-black">Last date : {lastDateToApply}</span>
+        </div>
+
+        <div className="">
+            <button className="px-2 py-2 text-sm font-semibold bg-secondary text-white whitespace-nowrap">View Details</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default ListItem;
